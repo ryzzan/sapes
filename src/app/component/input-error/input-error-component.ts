19824c6592edd27d26d1466e295fc450c2781232
Leonardo Victor <leonardovff@gmail.com>
2017-02-06 13:53:05 -0300

    Refactory nas mensagens de validação de input

--- src/app/component/input-error/input-error-component.ts
+++ src/app/component/input-error/input-error-component.ts
@@ -1,18 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'input-error',
   templateUrl: './input-error.component.html',
   styleUrls: ['./input-error.component.css']
 })
-export class InputErrorComponent implements OnInit {
-  @Input() control:any;
-  @Input() errorDefs:any;
+export class InputErrorComponent implements OnChanges {
+  @Input() validators:any = {};
+  @Input() errors:any;
+  @Input() title:string;
+  @Input() triedSend: boolean = false;
+  private messages:any;
+  private messageError: string = "";
 
   constructor() {
   }
-  ngOnInit() {
-    console.log(this.control);
+
+  ngOnInit(){
+    this.setMessages(this.validators);
   }
 
+  ngOnChanges(){
+    if(this.messages!=undefined){
+      this.messageError = this.getMessage(this.errors,this.messages);
+    }
+  }
+
+  getMessage(errors = {}, messages){
+    if(errors == null) return '';
+
+    let key = this.getError(errors);
+    if(key == "") return '';
+    return messages[key].replace('@field', this.title);
+  }
+
+  getError(errors){
+    if(typeof(errors.required)!="undefined")
+      return 'required';
+
+    let keys = Object.keys(errors);
+    if(keys.length === 0) '';
+
+    let key = null;
+    keys.forEach(function(value){
+      if(value != "required"){
+        return key = value;
+      }
+    })
+    if(key != null) return key;
+    return 'defaultInvalid';
+  }
+
+  setMessages({
+      required = 'O campo @field é obrigatório',
+      defaultInvalid = 'O valor do campo @field é inválido',
+      validateCpf = 'CPF em formato incorreto'
+  } = {}){
+      this.messages = {
+        required: required,
+        defaultInvalid: defaultInvalid,
+        validateCpf: validateCpf
+      }
+  }
 }

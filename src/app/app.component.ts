85aa310506052dc8b94a8735d9b65bcf53654838
Leonardo Victor <leonardovff@gmail.com>
2017-02-10 13:15:07 -0300

    Adição do service corporate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,6 @@ import { CorporateService } from './shared/corporate.service'
 export class AppComponent {
   title = 'app works!';
   constructor(private corporateService: CorporateService){
-    console.log('entrou');
-    this.corporateService.getData()
-      .subscribe( data =>{
-        console.log("entrou2");
-        console.log(data);
-      },
-      response => {
-        console.log("entrou", response);
-      });
+    this.corporateService.getToken();
   }
 }

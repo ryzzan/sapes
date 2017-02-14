8f20095b6f477b59d4f6c252ee7017ca826175e8
Leonardo Victor <leonardovff@gmail.com>
2017-02-14 16:10:13 -0300

    Correção dos relacionamentos do corporate service

--- src/app/shared/corporate.service.ts
+++ src/app/shared/corporate.service.ts
@@ -69,10 +69,10 @@ export class CorporateService {
       gender: student['sexo'],
       ethnicity_id: student.cd_raca_cor == null ? null : bdInfo.ethnicities.filter(
           ethnicity => ethnicity.code == student.cd_raca_cor
-      ),
+      )['id'],
       disability_id: student.cd_necessidade_especial == null ? null : bdInfo.disabilities.filter(
           disability => disability.code == student.cd_necessidade_especial
-      ),
+      )['id'],
       address: student.endereco,
       address_zip_code: student.cep,
       address_number: student.numero,
@@ -87,19 +87,21 @@ export class CorporateService {
         regional: curso.dr,
         unit_id: curso.cd_unidade == null ? null : bdInfo.units.filter(
           unit => unit.code == curso.cd_unidade
-        ),
+        )['id'],
         origin_id: curso.cd_escola_orig_aluno_no_curso == null ? null : bdInfo.origins.filter(
-          origin => origin.id == curso.cd_escola_orig_aluno_no_curso
-        ),
-        modality_id: curso.cd_modalidade,
+          origin => origin.code == curso.cd_escola_orig_aluno_no_curso
+        )['id'],
+        modality_id: curso.cd_modalidade == null ? null : bdInfo.modalities.filter(
+          modality => modality.id == curso.cd_modalidade
+        )['id'],
         distance_education: curso.ead!="N"? 1: 0,
         regimental_gratuity: curso['gratuidade_regimental']!="N"? 1: 0,
         occupation_id: curso.cd_ocupacao == null ? null : bdInfo.occupations.filter(
           occupation => occupation.code == curso.cd_ocupacao
-        ),
+        )['id'],
         area_id: curso.cd_area_atuacao == null ? null : bdInfo.areas.filter(
           area => area.code == curso.cd_area_atuacao
-        ),
+        )['id'],
         start_month: this.dateForCourse(curso.dt_inicio, 1),
         start_year: this.dateForCourse(curso.dt_inicio, 2),
         end_month: this.dateForCourse(curso.dt_termino, 1),

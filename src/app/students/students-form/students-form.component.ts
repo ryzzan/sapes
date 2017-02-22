5a9d62d8554a89f8fceaa68ab1a98273fc8ffdcf
Leonardo Victor <leonardovff@gmail.com>
2017-02-22 09:41:14 -0300

    Adição de id da resposta na edição do formulario do usuario

--- src/app/students/students-form/students-form.component.ts
+++ src/app/students/students-form/students-form.component.ts
@@ -236,8 +236,13 @@ export class StudentsFormComponent implements OnInit {
     var result,
     userValue = Object.assign(this.steps[0].value,this.steps[1].value,this.steps[2].value);
     userValue.answers = this.steps[3].value.concat(this.steps[4].value);
-
     userValue.answers.forEach( (answer, index) => {
+      let id = this.student.answers.filter(answer =>
+        answer.question_id == index + 1
+      );
+      if(typeof(id[0])!="undefined"){
+        answer.id = id[0]['id'];
+      }
       if(typeof(answer.alternative_flag)!="undefined"){
         answer.alternative_id = answer.alternative_flag ? 1 : 2;
         delete answer.alternative_flag;

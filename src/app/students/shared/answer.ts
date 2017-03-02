83fcab47703e1795376497532ff8a92474aca6a4
Leonardo Victor <leonardovff@gmail.com>
2017-03-02 17:05:39 -0300

    Melhora do botão de adicionar

--- src/app/students/shared/answer.ts
+++ src/app/students/shared/answer.ts
@@ -2,7 +2,7 @@ export interface Answer {
   id: number;
   alternative_id: number;
   alternative_flag: boolean;
-  complement: number;
+  complement: string;
   phase: number;
   question_id: number;
   student_id: number;

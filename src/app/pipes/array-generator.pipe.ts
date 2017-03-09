c2f6e9ce152f4ddfb66341ed0886a8c8d6db8b81
Ryzzan <rabbadesalman@gmail.com>
2017-03-09 17:04:32 -0300

    Atualização em students-list

--- src/app/pipes/array-generator.pipe.ts
+++ src/app/pipes/array-generator.pipe.ts
@@ -6,7 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ArrayGeneratorPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+
+    let arr = [];
+    for(let i = 0; i < value; i++) {
+      arr.push(i+1);
+    }
+    return arr;
   }
 
 }

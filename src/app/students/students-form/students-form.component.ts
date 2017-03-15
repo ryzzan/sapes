f27e0b53c2d7ee0c0ec565ae56eb000cceb779d0
Leonardo Victor <leonardovff@gmail.com>
2017-03-15 14:37:04 -0300

    Correção do indice inicial do formulario

--- src/app/students/students-form/students-form.component.ts
+++ src/app/students/students-form/students-form.component.ts
@@ -39,7 +39,7 @@ export class StudentsFormComponent implements OnInit {
   units: any = bdInfo.units;
   formPagination: any = {
     maxIndex: 4,
-    index: 1
+    index: 0
   };
   autoCorrectedDatePipe = autoCorrectedDatePipe;
   triedSend: boolean = false;
